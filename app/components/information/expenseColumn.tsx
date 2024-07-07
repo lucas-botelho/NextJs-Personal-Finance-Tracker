de3f5e522d92f057b8d1ce3d8374fbb66b664ba3
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2024-07-07 19:35:10 +0100

    expenses sorted by date

--- app/components/information/expenseColumn.tsx
+++ app/components/information/expenseColumn.tsx
@@ -18,7 +18,6 @@ const ExpenseColumn: React.FC<ExpenseColumnProps> = ({ title, userID, atom }) =>
     const formatDate = (timestamp: Timestamp) => {
         const date = new Date(timestamp.seconds * 1000);
         const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0');
         return `${day}`;
     };
 
@@ -76,16 +75,21 @@ const ExpenseColumn: React.FC<ExpenseColumnProps> = ({ title, userID, atom }) =>
                     <span className='text-center w-5/12'>Amount</span>
                     <span className='text-center w-2/12'>Date</span>
                 </div>
-                {expensesState.expenses.map((expense, index) => {
-                    total += expense.amount;
-                    return (
-                        <li key={index} className='flex p-2'>
-                            <span className='text-left text-gray-800 expense-name w-5/12'>{expense.title}</span>
-                            <span className='text-gray-600 expense-value w-5/12'>{expense.amount}  &euro;</span>
-                            <span className='text-gray-600 w-2/12'>{formatDate(expense.date)}</span>
-                        </li>
-                    );
-                })}
+                {expensesState.expenses &&
+                    expensesState.expenses
+                        .slice()
+                        .sort((a, b) => a.date.toDate().getTime() - b.date.toDate().getTime())
+                        .map((expense, index) => {
+                            total += expense.amount;
+                            return (
+                                <li key={index} className='flex p-2'>
+                                    <span className='text-left text-gray-800 expense-name w-5/12'>{expense.title}</span>
+                                    <span className='text-gray-600 expense-value w-5/12'>{expense.amount}  &euro;</span>
+                                    <span className='text-gray-600 w-2/12'>{formatDate(expense.date)}</span>
+                                </li>
+                            );
+                        })
+                }
             </ul>
             <span className='border-solid border-t-2 w-64 m-2'></span>
             <div className='font-bold mt-auto self-center'>Total: {total.toFixed(2)} &euro;</div>
